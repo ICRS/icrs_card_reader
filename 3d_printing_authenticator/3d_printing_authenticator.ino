040bf7c1d18e0289863c89af9d9bfd5482cd64ea
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-03-18 19:21:10 +0000

    Update 3d_printing_authenticator.ino
    added leds

--- 3d_printing_authenticator/3d_printing_authenticator.ino
+++ 3d_printing_authenticator/3d_printing_authenticator.ino
@@ -6,36 +6,74 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
+#include <FastLED.h>
+
 #include "config.h"
+
+#define NUM_LEDS 2
 
 PN532_I2C pn532_i2c(Wire);
 NfcAdapter nfc = NfcAdapter(pn532_i2c);
 String tagId = "None";
 byte nuidPICC[4];
 
+#define DATA_PIN 13
+CRGB leds[NUM_LEDS];
 
-void setup() { 
+void setup() {
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
+
   Serial.begin(9600);
   nfc.begin();
   // Setup WiFi
   initWiFi();
-
+  pinMode(DATA_PIN, OUTPUT);
 }
- 
+
 void loop() {
   readNFC();
 }
 
-void readNFC() 
+void blink(const CRGB::HTMLColorCode colour) 
 {
- if (nfc.tagPresent())
- {
-   NfcTag tag = nfc.read();
-   tag.print();
-   tagId = tag.getUidString();
-   postIDToServer(tagId);
- }
- delay(50);
+  for(int i =0; i < 5; i++) 
+  {
+    for(int j = 0; j < NUM_LEDS; j++) 
+    {
+      leds[j] = colour;      
+    }
+    FastLED.show();
+
+    delay(500);
+    for(int j = 0; j < NUM_LEDS; j++) 
+    {
+      leds[j] = CRGB::Black;
+    }
+    FastLED.show();
+
+    delay(500);
+    
+  }
+
+}
+
+void readNFC() {
+  if (nfc.tagPresent()) {
+    NfcTag tag = nfc.read();
+    tag.print();
+    tagId = tag.getUidString();
+    bool status = postIDToServer(tagId);
+    Serial.println(status);
+    if (status) 
+    {
+      blink(CRGB::Green);
+    } else 
+    {
+      blink(CRGB::Red);
+    }
+    // Serial.println(http)
+  }
+  delay(50);
 }
 
 void initWiFi() {
@@ -49,19 +87,22 @@ void initWiFi() {
   Serial.println(WiFi.localIP());
 }
 
-int postIDToServer(String id)
-{
-  if((WiFi.status() == WL_CONNECTED)) {
+int postIDToServer(String id) {
+  if ((WiFi.status() == WL_CONNECTED)) {
     Serial.println("Attempting to POST ID To Server");
     HTTPClient http;
 
     // configure traged server and url
     //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
     // http.begin(SERVER_IP, SERVER_PORT, "/addUser"); //HTTP
-    http.begin(SERVER_IP, SERVER_PORT, "/setPrintWindow"); //HTTP
+    http.begin(SERVER_IP, SERVER_PORT, "/setPrintWindow");  //HTTP
     http.addHeader("Content-Type", "application/json");
-  
-    return http.POST("{\"id\":\"" + id + "\", \"secret\":\"" + SECRET + "\"}");
+    http.addHeader("Accept", "*/*");
+
+    http.POST("{\"id\":\"" + id + "\", \"secret\":\"" + SECRET + "\"}");
+    // Serial.print("Status: ");
+    // Serial.println(http.getString());
+    return http.getString() == "SUCCESS";
   }
 
   return -1;
