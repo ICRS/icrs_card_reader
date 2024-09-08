cf2ddb5da69f8aaef79bcc4b898a038b156c4c50
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-09-08 20:54:41 +0100

    add neopixel indicator

--- label_printer/main.py
+++ label_printer/main.py
@@ -10,16 +10,22 @@ from PIL import Image
 from pn532pi import Pn532, pn532
 from pn532pi import Pn532I2c
 
+import board
+import neopixel
+
 from dotenv import load_dotenv
 load_dotenv()
 
 username = os.getenv("USERNAME", "username")
 password = os.getenv('PASSWORD', 'password')
 endpoint = os.getenv("ENDPOINT", 'localhost:8000')
+neopixel_pin = int(os.getenv("NEOPIXEL_PIN", 18))
 basic = HTTPBasicAuth(username, password)
 
 port = os.getenv("PORT", "/dev/ttyACM0")
 uuid = ""
+
+pixels = neopixel.NeoPixel(board.pin.Pin(neopixel_pin), 8)
 
 PN532_I2C = Pn532I2c(1)
 nfc = Pn532(PN532_I2C)
@@ -47,11 +53,20 @@ def setup():
     print("Waiting for an ISO14443A Card ...")
 
 
+def flash_red(pixels):
+    for _ in range(3):
+        pixels.fill(20, 0, 0)
+        time.sleep(0.05)
+        pixels.fill(0, 0, 0)
+        time.sleep(0.05)
+
+
 if __name__ == "__main__":
     transport = SerialTransport(port=port)
     setup()
     while True:
         time.sleep(0.1)
+        pixels.fill((0, 0, 0))
 
         success, uid = nfc.readPassiveTargetID(
             pn532.PN532_MIFARE_ISO14443A_106KBPS)
@@ -60,6 +75,7 @@ if __name__ == "__main__":
             u = uid.hex().upper()
             print("Uid: ", u)
             if uuid == u:
+                flash_red(pixels)
                 continue
 
             uuid = u
@@ -70,7 +86,9 @@ if __name__ == "__main__":
 
             if r.status_code != 200:
                 print(f"Bad response: {r.reason}")
+                flash_red(pixels)
                 continue
+
             j = r.json()
             image = Image.open(
                 io.BytesIO(
@@ -82,3 +100,5 @@ if __name__ == "__main__":
             density = 5
             printer = PrinterClient(transport)
             printer.print_image(image, density=density)
+        else:
+            flash_red(pixels)
